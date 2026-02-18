251c4e2f16a0975791977b1a8452d212ae208ed0
Kim Bui <ktb220000@utdallas.edu>
2026-02-17 20:44:11 -0600

    Set up Next.js with FastAPI

---
+++ yt-app/api/index.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
+
+@app.get("/api/healthchecker")
+def healthchecker():
+    return {"status": "success", "message": "Integrated FastAPI Framework with Next.js"}
